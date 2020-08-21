1ccb666e1e547ded0df70943eaa94c5beb053423
Cristina Altreche <c.altreche@gmail.com>
2020-08-21 10:00:49 -0400

    Stories configures and able to implement new stories.

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const path = require("path");
 const express = require("express");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const exphbs = require("express-handlebars");
 const passport = require("passport");
 const session = require("express-session");
+const MongoStore = require("connect-mongo")(session); //stores the login
 const morgan = require("morgan"); //For login
 
 // Load Config
@@ -17,6 +19,10 @@ connectDB(); //connects to mongoDB. Using db.js in config file.
 
 const app = express();
 
+// Body parser - allows us to GET the data (req.body)
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
 // Logging
 //this allows us to use morgan to display login attempts inside the console.
 //Only in Development
@@ -24,8 +30,12 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+
+// Handlebars Helpers - must be above handlebars
+const { formatDate } = require('./helpers/hbs')
+
 // Handlebars
-app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.engine(".hbs", exphbs({ helpers: {formatDate}, defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", ".hbs");
 
 // Sessions (make sure above passport middleware)
@@ -34,6 +44,7 @@ app.use(
     secret: "secret",
     resave: false,
     saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }), //to test this login go to dashboard. When you refresh page you shouldn't logout but instead persist. You can also see in your Mongo Database there is now a sessions storage
   })
 );
 
@@ -47,6 +58,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // Routes
 app.use("/", require("./routes/index"));
 app.use("/auth", require("./routes/auth"));
+app.use("/stories", require("./routes/stories"));
 
 // PORT
 const PORT = process.env.PORT || 3000;
